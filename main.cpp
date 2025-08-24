01b13b46524bcc389286576c7dd4b97066172eaa
hawda-dev <hadihawda566@gmail.com>
2025-08-24 23:15:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <windows.h>
-#include <iostream>
+
 
 int main() {
-    std::cout << "made by hawda.\n";
-    std::cout << "24/8/2025.\n\n";
+    
 
     while (true) {
-        STARTUPINFO si = { sizeof(si) };
+        STARTUPINFO si;
         PROCESS_INFORMATION pi;
+        ZeroMemory(&si, sizeof(si));
+        si.cb = sizeof(si);
+        si.dwFlags = STARTF_USESHOWWINDOW;
+        si.wShowWindow = SW_MINIMIZE; // Open minimized
 
-        // Try to launch Task Manager
         if (CreateProcess(
                 NULL,
                 (LPSTR)"taskmgr.exe", // Program to execute
@@ -17,17 +19,17 @@ int main() {
                 0, NULL, NULL,
                 &si, &pi))
         {
-            std::cout << "fixing.. \n";
+            
 
-            // Close process and thread handles to avoid leaks
+            // Close handles to prevent leaks
             CloseHandle(pi.hProcess);
             CloseHandle(pi.hThread);
         } 
         else {
-            std::cerr << "Error: " << GetLastError() << "\n";
+            
         }
 
-        // Wait 30 seconds before repeating
+      
         Sleep(15000);
     }
 
